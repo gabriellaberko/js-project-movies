b03017a52deed0752e801d56f1a1220daa6d7f81
Carolina Oldertz <carolina@Carolinas-MacBook-Air.local>
2025-12-18 00:03:48 +0100

    fetched genre names to home page from endpoint, so it can be connected with filtering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,18 @@ import { Loader } from "../components/assets/Loader";
 
 export const Home = () => {
 
-  const[popularMovies, setPopularMovies] = useState([]);
-  const[loading, setLoading] = useState(true);
-  const[error, setError] = useState(false);
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [genres, setGenres] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
+    const APIkey = `269615d5c201f031cd098191b590c560`;
+
     const fetchPopularMovies = async () => {
-      const APIkey = `269615d5c201f031cd098191b590c560`;
-      try{
+      try {
         const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${APIkey}&language=en-US&page=1`);
-        if(!response.ok){
+        if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json();
@@ -21,20 +23,38 @@ export const Home = () => {
         console.log(data.results);
         setPopularMovies(data.results);
       }
-      catch(error) {
+      catch (error) {
         console.log("Fetch error:", error);
         setError(true);
       }
     };
+
+    const fetchGenres = async () => {
+      try {
+        const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${APIkey}&language=en-US`)
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+        const data = await response.json()
+        setLoading(false)
+        console.log(data.genres)
+        setGenres(data.genres)
+      } catch (error) {
+        console.log("Fetch error:", error);
+        setError(true);
+      }
+    }
+
     fetchPopularMovies();
-  },[])
-  
+    fetchGenres();
+  }, [])
+
   return (
     <>
       <h1>Movies</h1>
       {loading && <Loader />}
       {error && <p>Data is unavailable right now. Try again later!</p>}
-      <CardContainer popularMovies={popularMovies}/>
+      <CardContainer popularMovies={popularMovies} />
     </>
   )
 }
